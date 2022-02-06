26c28e95e5cf2ec37c31c2e1abdc4fed520727a9
TzeKen <tantzeken05@gmail.com>
2022-02-06 04:04:25 +0000

    commit2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,243 @@
 <script>
-    import EntryModal from '$lib/EntryModal.svelte';
     import supabase from '$lib/db';
+import { each } from 'svelte/internal';
 
     async function signOut() {
    	 const { error } = await supabase.auth.signOut();
 
    	 if (error) alert(error.message); // alert if error
     }
+    index.svelte
+  let timetable = {
+	Monday: [
+  	{
+    	name: "PH",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "R",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Tuesday: [
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "E",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+	],
+	Wednesday: [
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "H",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "PKS",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Thursday: [
+  	{
+    	name: "SA",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "A",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Friday: [
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "T",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "SA",
+    	period: 2,
+    	style: "",
+  	},
+	],
+  };
+
+
+
 </script>
+<h1>My Dashboard</h1>
+<h2>My school timetable</h2>
+<table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">4</th>
+        <th scope="col">5</th>
+        <th scope="col">6</th>
+        <th scope="col">7</th>
+        <th scope="col">8</th>
+        <th scope="col">9</th>
+        <th scope="col">10</th>
+        <th scope="col">11</th>
+        <th scope="col">12</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Monday</th>
+        {#each timetable.Monday as timeslot,index}
+        <td colspan="timeslot.period" class="timeslot.style"></td>
+        {each}
+      </tr>
+      <tr>
+        <th scope="row">Tuesday</th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Wednesday</th>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Thursday</th>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Friday</th>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
 
 
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
-
-<EntryModal />
 
